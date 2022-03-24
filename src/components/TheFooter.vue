1aa947e8786d3349f970e6106e1d5cf06f28655a
albertofanini-prog <albertofanini@outlook.com>
2022-03-24 12:59:27 +0100

    Footer, hover

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -2,12 +2,22 @@
     <footer>
         <div class="info_wrapper">
             <div class="address_wrapper">
-                <h6>
-                    Address
-                </h6>
-                <p>382 NE 191st St # 87394 Miami, FL 33179-3899</p>
-                <p>+1 (305) 547-9909 (9am - 5pm EST, Monday - Friday)</p>
-                <p>support@maxcoach.com</p>
+                <div class="one">
+                    <h6>
+                        Address
+                    </h6>
+                    <ul>
+                        <li>382 NE 191st St # 87394 Miami, FL 33179-3899</li>
+                        <li>+1 (305) 547-9909 (9am - 5pm EST, Monday - Friday)</li>
+                        <li>support@maxcoach.com</li>
+                        <li>
+                            <img src="../assets/Icons/facebook-brands.svg" alt="">
+                            <img src="../assets/Icons/twitter-brands.svg" alt="">
+                            <img src="../assets/Icons/instagram-brands.svg" alt="">
+                            <img src="../assets/Icons/linkedin-brands.svg" alt="">
+                        </li>
+                    </ul>
+                </div>
             </div>
             <div class="links_wrapper">
                 <div class="first">
@@ -33,7 +43,10 @@
             </div>
         </div>
         <div class="rights_banner">
-
+            <div class="rights_text">
+                <h4>2020 Maxcoach. All Rights Reserved</h4>
+                <button><img src="../assets/Icons/up-long-solid.svg" alt=""></button>
+            </div>
         </div>
     </footer>
 </template>
@@ -48,7 +61,7 @@ export default {
     footer{
         width: 100%;
         height: 400px;
-        border: 7px solid red;
+        // border: 7px solid red;
         display: flex;
         flex-direction: column;
         align-items: center;
@@ -57,40 +70,66 @@ export default {
         .info_wrapper{
             width: 70%;
             height: 100%;
-            border: 5px solid yellow;
+            // border: 5px solid yellow;
             display: flex;
             align-items: center;
             color: #8b8b90;
             .address_wrapper{
                 width: 50%;
-                height: 60%;
-                border: 3px solid green;
+                height: 100%;
+                // border: 3px solid green;
                 display: flex;
-                flex-direction: column;
-                align-items: flex-start;
-                justify-content: center;
+                align-items: center;
                 text-align: left;
-                h6{
-                    color: white;
-                    font-size: 22px;
+                .one{
                     width: 100%;
-                    height: 40%;
-                }
-                p{
-                    font-size: 16px;
-                    width: 100%;
-                    height: 20%;
+                    height: 60%;
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    padding: 5px 10px;
+                    h6{
+                        color: white;
+                        font-size: 22px;
+                        width: 100%;
+                        height: 20%;
+                        margin-bottom: 25px;
+                    }
+                    p{
+                        font-size: 16px;
+                        width: 100%;
+                        height: 20%;
+                    }
+                    ul{
+                        width: 100%;
+                        height: 80%;
+                        list-style: none;
+                        display: flex;
+                        flex-direction: column;
+                        li{
+                            width: 100%;
+                            font-size: 16px;
+                            height: 25%;
+                            img{
+                                width: 35px;
+                                height: 35px;
+                                padding: 0px 5px;
+                                filter: invert(54%) sepia(0%) saturate(1581%) hue-rotate(352deg) brightness(102%) contrast(93%);
+                                cursor: pointer;
+                            }
+                        }
+                    }
                 }
             }
             .links_wrapper{
                 width: 50%;
                 height: 100%;
-                border: 3px solid green;
+                // border: 3px solid green;
                 display: flex;
                 align-items: center;
                 padding: 0px 20px;
                 .first{
-                    border: 1px solid red;
+                    // border: 1px solid red;
                     width: 60%;
                     height: 60%;
                     list-style: none;
@@ -119,7 +158,7 @@ export default {
                     }
                 }
                 .second{
-                    border: 1px solid red;
+                    // border: 1px solid red;
                     width: 40%;
                     height: 60%;
                     list-style: none;
@@ -152,7 +191,36 @@ export default {
         .rights_banner{
             width: 70%;
             height: 25%;
-            border: 5px solid yellow;
+            // border: 5px solid yellow;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            position: relative;
+            .rights_text{
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                color: #8b8b90;
+                button{
+                    position: absolute;
+                    top: 0;
+                    right: 0;
+                    transform: translate(500%, 0%);
+                    height: 50px;
+                    width: 50px;
+                    border-radius: 999px;
+                    border: none;
+                    background-color: #1fad95;
+                    display: flex;
+                    align-items: center;
+                    cursor: pointer;
+                    img{
+                        width: 80%;
+                        height: 60%;
+                        filter: invert(100%) sepia(100%) saturate(2%) hue-rotate(22deg) brightness(107%) contrast(101%);
+                    }
+                }
+            }
         }
     }
 </style>
